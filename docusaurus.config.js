ec795eb7fc901ac3d5f6bb0221348195fc3b9f1f
Hsuan Han Lai <hsuan.han.lai@gmail.com>
2025-05-22 16:05:05 +0800

    try new build path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   url: 'https://tuyaopen.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/website/',
 
   // https://github.com/tuya/TuyaOpen
 
